01df0d305a682d292058c7efd78a063ccc20e44d
lindsayMecher <lindsaymecher@gmail.com>
2020-10-15 16:05:54 -0500

    finished deliverables

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+
+    has_many :employees
+
+    def self.sort_by_employees 
+        self.all.sort_by do |dog|
+            dog.employees.count
+        end.reverse 
+    end 
+
 end
